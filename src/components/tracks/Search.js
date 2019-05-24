864b17784f81ed3f87d9173356e7ac3671ae1ea4
aeolion7 <joseph.lionarons@cedrus.digital>
2019-05-24 16:21:10 -0400

    added controlled form Search component

---
+++ src/components/tracks/Search.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Consumer } from '../../context';
+
+class Search extends Component {
+  state = {
+    trackTitle: '',
+  };
+
+  onChange = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          return (
+            <div className="card card-body mb-4 p-4">
+              <h1 className="display-4 text-center">
+                <i className="fas fa-music mr-3" />
+                Search for a song:
+              </h1>
+              <p className="lead text-center">Get the lyrics for any song.</p>
+              <form>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Insert song title..."
+                    name="trackTitle"
+                    value={this.state.trackTitle}
+                    onChange={this.onChange}
+                  />
+                </div>
+              </form>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Search;
