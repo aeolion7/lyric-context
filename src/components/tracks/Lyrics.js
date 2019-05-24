fcf89df55bd2ccf309c2e880c9d1a876d6f3ee44
aeolion7 <joseph.lionarons@cedrus.digital>
2019-05-24 16:06:28 -0400

    fixed music genre display bug

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -12,31 +12,44 @@ class Lyrics extends Component {
   };
 
   async componentDidMount() {
-    const { id } = this.props.match.params;
+    try {
+      const { id } = this.props.match.params;
 
-    const { data } = await axios.get(
-      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${
-        process.env.REACT_APP_MM_KEY
-      }`
-    );
+      const { data } = await axios.get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      );
 
-    this.setState({
-      lyrics: data.message.body.lyrics,
-    });
+      this.setState({
+        lyrics: data.message.body.lyrics,
+      });
 
-    const trackResponse = await axios.get(
-      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${id}&apikey=${
-        process.env.REACT_APP_MM_KEY
-      }`
-    );
+      const trackResponse = await axios.get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${id}&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      );
 
-    this.setState({
-      track: trackResponse.data.message.body.track,
-    });
+      this.setState({
+        track: trackResponse.data.message.body.track,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render() {
     const { track, lyrics } = this.state;
+    const {
+      track_name,
+      artist_name,
+      album_id,
+      first_release_date,
+      explicit,
+      primary_genres,
+    } = track;
+    const { lyrics_body } = lyrics;
 
     if (Object.keys(track).length === 0 || Object.keys(lyrics).length === 0) {
       return <Spinner />;
@@ -48,31 +61,29 @@ class Lyrics extends Component {
           </Link>
           <div className="card">
             <h5 className="card-header">
-              {track.track_name} by{' '}
-              <span className="secondary">{track.artist_name}</span>
+              {track_name} by <span className="secondary">{artist_name}</span>
             </h5>
             <div className="card-body">
-              <p className="card-text">{lyrics.lyrics_body}</p>
+              <p className="card-text">{lyrics_body}</p>
             </div>
           </div>
           <ul className="list-group mt-3">
             <li className="list-group-item">
-              <strong>Album ID</strong>: {track.album_id}
+              <strong>Album ID</strong>: {album_id}
             </li>
             <li className="list-group-item">
               <strong>Genre</strong>:{' '}
-              {
-                track.primary_genres.music_genre_list[0].music_genre
-                  .music_genre_name
-              }
+              {primary_genres.music_genre_list[0]
+                ? primary_genres.music_genre_list[0].music_genre
+                    .music_genre_name
+                : 'N/A'}
             </li>
             <li className="list-group-item">
-              <strong>Explicit Lyrics</strong>:{' '}
-              {track.explicit === 0 ? 'No' : 'Yes'}
+              <strong>Explicit Lyrics</strong>: {explicit === 0 ? 'No' : 'Yes'}
             </li>
             <li className="list-group-item">
               <strong>Release Date</strong>:{' '}
-              <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
+              <Moment format="MM/DD/YYYY">{first_release_date}</Moment>
             </li>
           </ul>
         </>
--- src/context.js
+++ src/context.js
@@ -15,14 +15,18 @@ export class Provider extends Component {
   };
 
   async componentDidMount() {
-    const { data } = await axios.get(
-      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
-        process.env.REACT_APP_MM_KEY
-      }`
-    );
-    this.setState({
-      track_list: data.message.body.track_list,
-    });
+    try {
+      const { data } = await axios.get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      );
+      this.setState({
+        track_list: data.message.body.track_list,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render() {
