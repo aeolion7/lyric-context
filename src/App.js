3916b70d772c4b7b6a3a52c1e5269eac188b2135
aeolion7 <joseph.lionarons@cedrus.digital>
2019-05-24 12:43:14 -0400

    implemented context and connected to external API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from './context';
 
-function App() {
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <Router>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/context.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const Context = React.createContext();
+
+/*
+  The Provider is just like any other React component.
+  Everything gets wrapped within the Provider.
+*/
+
+export class Provider extends Component {
+  state = {
+    track_list: [],
+    heading: 'Top 10 Tracks',
+  };
+
+  async componentDidMount() {
+    const { data } = await axios.get(
+      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+        process.env.REACT_APP_MM_KEY
+      }`
+    );
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {/*
+          The Provider wraps around every other component, so that whatever
+          state is put inside the Provider is accessible within all other
+          components so long as a Consumer is used.
+        */}
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+/*
+  The Consumer is what is imported into a component in order to be able to
+  access the state within the component, similar to how connect works in Redux.
+*/
+export const Consumer = Context.Consumer;
